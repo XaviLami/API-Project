d5a42b80c37c1f92adadbd79f2bb56c067c0bfc9
LAMI <xavier.lami@edu.devinci.fr>
2019-12-05 20:00:26 +0100

    Add API Football

--- js/script2.js
+++ js/script2.js
@@ -1,24 +1,29 @@
-function getApiData() {
-    const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            const data = JSON.parse(xhr.responseText);
-            
-            const name=data.name; 
+function documentAdd(){
 
-            var firstname=document.getElementById(firstname).value;
-            // Inserer id de firstname
-            
-            var lastname=document.getElementById(lastname).value;
+    
+    var lastname=document.getElementById('lastname').value;
+   
+    var firstname=document.getElementById('firstname').value;
+    
+   
 
-            console.log(name);
+   var url= "https://apiv2.apifootball.com/?action=get_players&player_name="+lastname+' '+firstname+"&APIkey=9fcbb3a5d4186964a9ed7ca00436dd5d8ad73b012662543323acee31e46b9f09";
 
-            var url= "https://nba-players.herokuapp.com/players-stats/"+firstname+'/'+lastname;
-            
-       
-        }
-    }
-    xhr.open('GET',);
-    xhr.send()
+   //var url ="https://apiv2.apifootball.com/?action=get_players&player_name=messi lionel&APIkey=9fcbb3a5d4186964a9ed7ca00436dd5d8ad73b012662543323acee31e46b9f09"
+
+
+   fetch(url).then(function (resp) {return resp.json()}).then(function (data) {
+
+   console.log(data)
+   console.log(data.team_name)
+
+   
+   document.querySelector('div.Age').innerHTML = 'Age : '+ data.player_age;
+   document.querySelector('div.NameTeam').innerHTML = "Nom d'équipe : " + data.team_name;
+   document.querySelector('div.Number').innerHTML = 'Numéro : '+ data.player_number;
+
+   })
 }
- getApiData()
+  
+
+     
